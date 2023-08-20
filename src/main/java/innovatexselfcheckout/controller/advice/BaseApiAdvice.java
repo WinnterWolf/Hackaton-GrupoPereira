a46dd167b625dc00ad79b9b210d7b1e8a43783ed
Patrick Borges <patborges@uolinc.com>
2023-08-20 17:35:37 -0300

    fix: refactoring controller advice

--- src/main/java/innovatexselfcheckout/controller/advice/BaseApiAdvice.java
+++ src/main/java/innovatexselfcheckout/controller/advice/BaseApiAdvice.java
@@ -2,34 +2,36 @@ package innovatexselfcheckout.controller.advice;
 
 import innovatexselfcheckout.exceptions.CustomerNotFoundException;
 import innovatexselfcheckout.exceptions.InvalidCpfException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class BaseApiAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ CustomerNotFoundException.class })
-    public ResponseEntity<Object> handleCustomerNotFoundException(final RuntimeException ex){
+    public ResponseEntity<Object> handleCustomerNotFoundException(final RuntimeException ex, final WebRequest request){
         return super.handleExceptionInternal(
                 ex,
                 ex.getMessage(),
-                null,
+                HttpHeaders.EMPTY,
                 HttpStatus.NOT_FOUND,
-                null
+                request
         );
     }
 
     @ExceptionHandler({ InvalidCpfException.class })
-    public ResponseEntity<Object> handleInvalidCpfException(final RuntimeException ex){
+    public ResponseEntity<Object> handleInvalidCpfException(final RuntimeException ex, final WebRequest request){
         return super.handleExceptionInternal(
                 ex,
                 ex.getMessage(),
-                null,
+                HttpHeaders.EMPTY,
                 HttpStatus.BAD_REQUEST,
-                null
+                request
         );
     }
 }
