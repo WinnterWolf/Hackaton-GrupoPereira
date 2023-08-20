9538080d6e9013dcb12c64cb7990ff53bc339019
Karol <karolinessilva@live.com>
2023-08-20 12:43:48 -0400

    Revert "Revert "criacao de novos metodos""
    This reverts commit 34e5733cb20e783f3fcb51ea0c85092aa205ce1f.

--- src/main/java/innovatexselfcheckout/controller/ShoppingController.java
+++ src/main/java/innovatexselfcheckout/controller/ShoppingController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 @RestController
 @RequestMapping(path = "v1/shopping")
 public class ShoppingController {
@@ -17,9 +20,20 @@ public class ShoppingController {
 
     @PostMapping
     public ResponseEntity AdicionarCompra(@RequestBody Shopping shopping){
-        shoppingService.adicionarCompra(shopping);
+        var service = shoppingService.adicionarCompra(shopping);
+        if (!service){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Nao foi possivel cadastrar usuario");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Shopping> obterPorId(@PathVariable("cpf") BigInteger id){
 
+        var service = shoppingService.obterCompraPorId(id);
+        if (null == service){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 /*
