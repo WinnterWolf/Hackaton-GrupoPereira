b65bc294c554ca5ba55a1aacef796ecbde7beae2
Patrick Borges <patborges@uolinc.com>
2023-08-20 18:13:29 -0300

    fix: correcting swagger config location

--- src/main/java/innovatexselfcheckout/config/SwaggerConfig.java
+++ src/main/java/innovatexselfcheckout/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package innovatexselfcheckout;
+package innovatexselfcheckout.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
