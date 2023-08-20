d02bc929ad3a52bbf7205f0fcfaee4060b96ad75
Patrick Borges <patborges@uolinc.com>
2023-08-20 18:23:48 -0300

    feat: implementing Swagger doc on CustomerController

--- src/main/java/innovatexselfcheckout/controller/CustomerController.java
+++ src/main/java/innovatexselfcheckout/controller/CustomerController.java
@@ -3,6 +3,7 @@ package innovatexselfcheckout.controller;
 import innovatexselfcheckout.model.Customer;
 import innovatexselfcheckout.service.ICustomerService;
 import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +19,19 @@ public class CustomerController {
     @Autowired
     private ICustomerService customerService;
 
+    @Operation(summary = "Obtem todos os customers cadastrados")
     @GetMapping
     public ResponseEntity<List<Customer>> getCustomers(){
         return ResponseEntity.status(HttpStatus.OK).body(customerService.getAllCustomers());
     }
 
+    @Operation(summary = "Obtem um customer usando o cpf")
     @GetMapping("/{cpf}")
     public ResponseEntity<Customer> getCustomerByCpf(@PathVariable String cpf){
         return ResponseEntity.status(HttpStatus.OK).body(customerService.getCustomerByCpf(cpf));
     }
 
+    @Operation(summary = "Cadastra um customer")
     @PostMapping
     public ResponseEntity addCustomer(@RequestBody Customer customer){
 
@@ -39,6 +43,7 @@ public class CustomerController {
 
     }
 
+    @Operation(summary = "Deleta os dados de um customer usando o CPF")
     @DeleteMapping("/{cpf}")
     public ResponseEntity deleteCustomer(@PathVariable("cpf") String cpf){
         final Boolean customerResult = customerService.deleteCustomer(cpf);
