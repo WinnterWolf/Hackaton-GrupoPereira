976c6fe4ab790dbaa7cec238511127b7a4730c0b
Patrick Borges <patborges@uolinc.com>
2023-08-20 15:03:25 -0300

    fix: fixing mapper

--- src/main/java/innovatexselfcheckout/model/mapper/ProductMapper.java
+++ src/main/java/innovatexselfcheckout/model/mapper/ProductMapper.java
@@ -3,9 +3,12 @@ package innovatexselfcheckout.model.mapper;
 import innovatexselfcheckout.model.Product;
 import innovatexselfcheckout.repository.entity.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(target = "price", source = "preco")
+    @Mapping(target = "category", source = "categoria")
     ProductEntity toEntity(Product product);
 
     Product toProduct(ProductEntity entity);
